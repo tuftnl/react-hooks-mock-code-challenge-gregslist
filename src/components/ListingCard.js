69db943201755cd53a1a312694881f99f77724a4
Aaron Lin <aaron.l.lin.general@gmail.com>
2024-04-30 16:56:28 -0400

    craigslist mockup

--- src/components/ListingCard.js
+++ src/components/ListingCard.js
@@ -1,24 +1,40 @@
-import React from "react";
+import React, {useState} from "react";
 
-function ListingCard() {
+function ListingCard({ listing, removeListing }) {
+  console.log(listing)
+  const {id, description, image, location} = listing
+  const [fav, setFav] = useState(false)
+
+  function deleteListing() {
+    fetch(`http://localhost:6001/listings/${listing.id}`,
+  {method: 'DELETE'})
+  .then((resp) => {
+    if (resp.ok) {
+      removeListing(id)
+    } else {
+      console.log('Could not remove listing.')
+    }
+  })
+  }
+
   return (
     <li className="card">
       <div className="image">
         <span className="price">$0</span>
-        <img src={"https://via.placeholder.com/300x300"} alt={"description"} />
+        <img src={listing.image} alt={listing.description} />
       </div>
       <div className="details">
-        {true ? (
-          <button className="emoji-button favorite active">★</button>
+        {fav ? (
+          <button className="emoji-button favorite active" onClick={()=>setFav(!fav)}>★</button>
         ) : (
-          <button className="emoji-button favorite">☆</button>
+          <button className="emoji-button favorite" onClick={setFav}>☆</button>
         )}
-        <strong>{"description"}</strong>
-        <span> · {"location"}</span>
-        <button className="emoji-button delete">🗑</button>
+        <strong>{listing.description}</strong>
+        <span> · {listing.location}</span>
+        <button className="emoji-button delete" onClick={deleteListing}>🗑</button>
       </div>
     </li>
-  );
+  )
 }
 
 export default ListingCard;
--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,11 +1,32 @@
-import React from "react";
-// import ListingCard from "./ListingCard";
+import React, {useState, useEffect} from "react";
+import ListingCard from "./ListingCard";
 
 function ListingsContainer() {
+  const [listings, setListings] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:6001/listings')
+    .then((res) => res.json())
+    .then((data) => {
+      //console.log(data)
+      setListings(data)
+    })
+  }, [])
+
+  function removeListing(listingId) {
+    const filteredListings = listings.filter((listing) => listing.id !== listingId)
+    setListings(filteredListings)
+  }
+
   return (
     <main>
       <ul className="cards">
-        {/* use the ListingCard component to display listings */}
+        {
+        listings.map((listing) => {
+          return <ListingCard key={listing.id} listing={listing} removeListing={removeListing} />
+          //console.log(listing)
+        })
+        }
       </ul>
     </main>
   );
